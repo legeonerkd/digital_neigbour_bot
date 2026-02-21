2057d11e44e1e0ef4f40dee49f95219be3189f0b
legeonerkd <legeonerkd@gmai.com>
2026-02-21 09:59:52 +0200

    Initial commit: Digital Neighbour bot

---
+++ keyboards/main_menu.py
@@ -0,0 +1,11 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+MAIN_MENU = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="FAQ"), KeyboardButton(text="Полезные контакты")],
+        [KeyboardButton(text="Экстренная помощь"), KeyboardButton(text="Сервисы")],
+        [KeyboardButton(text="Мои напоминания"), KeyboardButton(text="Ближайшие напоминания")],
+        [KeyboardButton(text="Ближайшие сервисы")],
+    ],
+    resize_keyboard=True,
+)
